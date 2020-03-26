c7abd1a5df450e33b2fb3ab94e17c40091a668bd
ProNoobWarning <43936923+ProNoobWarning@users.noreply.github.com>
2020-03-26 13:51:32 +0200

    Ver. 1.5

--- Image.py
+++ Image.py
@@ -1,25 +1,23 @@
 import numpy as np
 import cv2 as cv
 
-def Mag(Img):
+def Comp(Img, o):
     f = np.fft.fft2(Img)
     f = np.fft.fftshift(f)
-    magnitude_spectrum = 20*np.log(np.abs(f))
-    return magnitude_spectrum
+    if  o == 0 or o == 1:
+        magnitude_spectrum = 20*np.log(np.abs(f))
+        return magnitude_spectrum
 
-def Phase(Img):
-    f = np.fft.fft2(Img)
-    f = np.fft.fftshift(f)
-    phase_spectrum = np.angle(f)
-    return phase_spectrum
+    if o == 2:
+        phase_spectrum = np.angle(f)
+        return phase_spectrum
 
-def Real(Img):
-    f = np.fft.fft2(Img)
-    f = np.fft.fftshift(f)
-    real_comp = 20*np.log(np.real(f))
-    return real_comp
+    if o == 3:
+        real_comp = 20*np.log(np.real(f))
+        return real_comp
 
-def Imag(Img):
-    f = np.fft.fft2(Img)
-    imag_comp = np.imag(f)
-    return imag_comp
+    if o == 4:
+        imag_comp = np.imag(f)
+        return imag_comp
+
+#def Mix()
--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-import UI
-from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
-import numpy as np
-from GetDataFromFile import getDataFromAFile
+
 import cv2 as cv
-from Image import Mag,Phase,Real,Imag
+import numpy as np
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+import UI
+from GetDataFromFile import getDataFromAFile
+from Image import Comp
 
 
 class ApplicationWindow(UI.Ui_MainWindow):
-    def __init__(self,mainWindow):
-        super(ApplicationWindow,self).setupUi(mainWindow)
-        
+    def __init__(self, mainWindow):
+        super(ApplicationWindow, self).setupUi(mainWindow)
+
         self.DisableMixer()
         self.Init()
 
@@ -24,31 +26,31 @@ class ApplicationWindow(UI.Ui_MainWindow):
         ]
 
         self.Combs = [
-            self.Img1_Comb,
-            self.Img1_Comb_2,
-            self.Output_Comb,
-            self.Comp1_CombS,
-            self.Comp1C_CombS,
-            self.Comp2_CombS,
+            self.Img1_Comb, self.Img1_Comb_2, self.Output_Comb,
+            self.Comp1_CombS, self.Comp1C_CombS, self.Comp2_CombS,
             self.Comp2C_CombS
         ]
-        
+
         self.i = 0
-        while(self.i < 6):
+        while (self.i < 6):
             self.Viewers[self.i].ui.histogram.hide()
             self.Viewers[self.i].ui.roiBtn.hide()
             self.Viewers[self.i].ui.roiPlot.hide()
             self.Viewers[self.i].ui.menuBtn.hide()
             self.i += 1
 
-        self.Upbtn1.clicked.connect(lambda: self.Disp(0,True))
-        self.Upbtn2.clicked.connect(lambda: self.Disp(1,True))
+        self.Upbtn1.clicked.connect(lambda: self.Disp(0, True))
+        self.Upbtn2.clicked.connect(lambda: self.Disp(1, True))
 
-        self.Img1_Comb.currentIndexChanged.connect(lambda: self.Disp(0,False))
-        self.Img1_Comb_2.currentIndexChanged.connect(lambda: self.Disp(1,False))
+        self.Img1_Comb.currentIndexChanged.connect(lambda: self.Disp(0, False))
+        self.Img1_Comb_2.currentIndexChanged.connect(
+            lambda: self.Disp(1, False))
 
     def Init(self):
-        self.ImgUp = [False,False]
+        self.ImgUp = [False, False]
+        self.Img1 = np.empty(0)
+        self.Img2 = np.empty(0)
+        self.Img = [self.Img1, self.Img2]
 
     def DisableMixer(self):
         self.Output_Comb.setEnabled(False)
@@ -68,49 +70,57 @@ class ApplicationWindow(UI.Ui_MainWindow):
         self.Slider2.setEnabled(True)
         self.Comp2C_CombS.setEnabled(True)
 
-    def GetImage(self,i):
-        filePath=QtWidgets.QFileDialog.getOpenFileName(None,  'load', "./","All Files *;")
-        if i == 0:
-            self.Img1 = getDataFromAFile(filePath) #if getDataFromAFile(filePath) is not None else self.MainData
-            self.Img2 = getDataFromAFile(filePath)
-        if i == 1:
-            self.Img2 = getDataFromAFile(filePath) #if getDataFromAFile(filePath) is not None else self.MainData
-        self.Img = [self.Img1,self.Img2]
-
+    def GetImage(self, i):
+        filePath = QtWidgets.QFileDialog.getOpenFileName(
+            None, 'load', "./", "All Files *;")
+        self.Img[i] = getDataFromAFile(filePath)
         if str(type(self.Img[i])) == "<class 'NoneType'>":
             self.ImgUp[i] = False
         else:
-            self.ImgUp[i] = True
+            if any(self.ImgUp):
+                if self.ImgUp[i] == False:
+                    if self.Img[0].shape == self.Img[1].shape:
+                        self.ImgUp[i] = True
+                    else:
+                        self.Img[i] = np.empty(0)
+                        self.Viewers[i].clear()
+                        self.Viewers[i + 2].clear()
+                        #todo: add popUp
+                elif (self.ImgUp[i] == True) and (self.ImgUp[0] == self.ImgUp[1]):
+                    if self.Img[0].shape == self.Img[1].shape:
+                        self.ImgUp[i] = True
+                    else:
+                        self.Img[i] = np.empty(0)
+                        self.ImgUp[i] = False
+                        self.Viewers[i].clear()
+                        self.Viewers[i + 2].clear()
+                        #todo: add popUp
+            else:
+                self.ImgUp[i] = True
 
-    def Disp(self,i,GI):
+    def Disp(self, i, GI):
         if GI == True:
             self.GetImage(i)
 
         if self.ImgUp[i] == True:
             self.Viewers[i].setImage(self.Img[i].T)
-            print(self.Img[i])
+            self.Viewers[i + 2].setImage(Comp(self.Img[i].T,self.Combs[i].currentIndex()))
             if self.Combs[i].currentIndex() == 0:
-                self.Viewers[i+2].setImage(Mag(self.Img[i].T))
                 self.Combs[i].setCurrentIndex(1)
-            if self.Combs[i].currentIndex() == 1:
-                self.Viewers[i+2].setImage(Mag(self.Img[i].T))
-            if self.Combs[i].currentIndex() == 2:
-                self.Viewers[i+2].setImage(Phase(self.Img[i].T))
-            if self.Combs[i].currentIndex() == 3:
-                self.Viewers[i+2].setImage(Real(self.Img[i].T))
-            if self.Combs[i].currentIndex() == 4:
-                self.Viewers[i+2].setImage(Imag(self.Img[i].T))
-        
 
-
+        if self.ImgUp[0] and self.ImgUp[1]:
+            self.EnableMixer()
+        else:
+            self.DisableMixer()
 
 
 def main():
-    app=QtWidgets.QApplication(sys.argv)
-    mainWindow=QtWidgets.QMainWindow()
+    app = QtWidgets.QApplication(sys.argv)
+    mainWindow = QtWidgets.QMainWindow()
     ApplicationWindow(mainWindow)
     mainWindow.show()
     sys.exit(app.exec_())
-    
+
+
 if __name__ == "__main__":
     main()
